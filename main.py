577e2a9bdae3fa4dab1bd61935af565741fbefaa
Gottfrid Olsson <gottfridolsson@hotmail.com>
2022-10-05 14:36:32 +0200

    Plots, update code

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 ##=======================================================##
 ##     Project: Atomic Probe Tomography [TIF295]
-##        File: APT_data_analysis_main.py
+##        File: main.py
 ##      Author: GOTTFRID OLSSON 
 ##     Created: 2022-09-21, 13:18
-##     Updated: 2022-09-21, 15:50
+##     Updated: 2022-10-05, 13:32
 ##       About: Analysis of data from APT lab.
-##              1. Read CSV files with Cr-RDF
-##              2. Normalize Cr-RDF
-##              3. Find wavelength (first max of Cr-RDF)
-##              4. Linear fit to find amplitude
-##              5. Print values
+##              1. Read CSV files with normalized Cr-RDF
+##              2. Find wavelength (first max of Cr-RDF)
+##              3. Linear fit to find amplitude
+##              4. Print values
 ##=======================================================##
 
 
@@ -51,7 +50,7 @@ def get_min_value_and_index(list):
     return min_val, min_val_index
 
 def amplitude_from_RDF(RDF_0, C_0):
-    return 2*C_0*np.sqrt(2*RDF_0-1)
+    return 2*C_0*np.sqrt(2*(RDF_0-1))
 
 
 
@@ -62,8 +61,7 @@ def amplitude_from_RDF(RDF_0, C_0):
 Plot = True
 
 # READ CSV #
-CURRENT_PATH  = os.path.abspath(os.path.dirname(__file__)
-)
+CURRENT_PATH  = os.path.abspath(os.path.dirname(__file__))
 CSV_PATH_10h  = '\Data from lab\RDF_10h_removedPole_dist10nm_step0,2nm_bulkNormalizedConc.csv'
 CSV_PATH_100h = '\Data from lab\RDF_100h_removedPole_dist10nm_step0,2nm_bulkNormalizedConc.csv'
 
@@ -107,17 +105,15 @@ lambda_100h['First minimum'] = 2*min_distance_100h # [nm]
 lambda_10h = {}
 lambda_10h['First maximum'] = max_distance_10h   # [nm]
 
-print("\nWavelength determined from RDF (100h):\n  From first maximum: " + str(lambda_100h['First maximum']) + " nm\n  From first minimum: " + str(lambda_100h['First minimum']) + " nm")
-print("\nWavelength determined from RDF (10h):\n  From first maximum: " + str(lambda_10h['First maximum']) + " nm")
 
 
-
-# AMPLITUDE FOR 100h #
-C_0 = [0.35452221537718685, 0.3520788658071025] #from "bulk concentration (ionic %)" of Cr in data files "RDF_*h_removedPole_dist10nm_step0,2nm.csv" from the lab
+# AMPLITUDE #
+C_0 = [35.452221537718685, 35.20788658071025] #[%], from "bulk concentration (ionic %)" of Cr in data files "RDF_*h_removedPole_dist10nm_step0,2nm.csv" from the lab
 amplitude = [0,0]
 n_first_values = [4, 5] #chosen based on what points of RDF we want to do linear regression to (plot and see)
 distance = [distance_10h, distance_100h]
 Cr_conc  = [Cr_conc_10h, Cr_conc_100h]
+linear_regression_equals_y_1 = [0,0]
 
 for i in range(2):
     x_data = np.array(distance[i][0:n_first_values[i]])
@@ -126,19 +122,28 @@ for i in range(2):
     m, k = linear_regression.solve_normal_equation(x_data, y_data, degree=1)
 
     if True:
-        x = np.linspace(0,1.5,10)
+        x_max = [1.3, 2]
+        x = np.linspace(0,x_max[i],1000)
         y = k*x + m
         plt.plot(distance_10h,  Cr_conc_10h,  marker='o', label='Cr-conc 10h')
         plt.plot(distance_100h, Cr_conc_100h, marker='x', label='Cr-conc 100h')
-        plt.plot(x, y,                        marker='s', label='linear least squares fit')
+        plt.plot(x, y,                        marker='.', label='linear least squares fit')
         plt.legend()
         plt.grid()
         plt.show()
-
+    ##print(i, f"y={m} for x=0", f"and y=0.5 for x={(0.5-m)/k}")
     RDF_0 = m #intercept y-axis at x=0
-    amplitude[i] = amplitude_from_RDF(RDF_0, C_0[i]) # [nm], = 2*A = 2*C_0*sqrt(2*RDF(0)-1)
+    amplitude[i] = amplitude_from_RDF(RDF_0, C_0[i]) # [%], = 2*A = 2*C_0*sqrt(2*RDF(0)-1)
+    linear_regression_equals_y_1[i] = (1-m) / k # y = k*x+m = 1
 
-print("\nCalculated amplitude for  10h is: " + str(amplitude[0]) + " nm")
-print("Calculated amplitude for 100h is: " + str(amplitude[1]) + " nm\n")
+print("\nCalculated amplitude for  10h is: " + str(amplitude[0]) + " %")
+print("Calculated amplitude for 100h is: " + str(amplitude[1]) + " %\n")
+
+print("\nWavelength determined from RDF (100h):\n  From first maximum: " + str(lambda_100h['First maximum']) + " nm\n  From first minimum: " + str(lambda_100h['First minimum']) + " nm")
+print("\nWavelength determined from RDF (10h):\n   From first maximum: " + str(lambda_10h['First maximum']) + " nm")
+
+print("\nWavelength determined from RDF (10h):\n   From linear regression, where line crosses RDF = 1: " + str(4*linear_regression_equals_y_1[0]) + " nm")
+print("\nWavelength determined from RDF (100h):\n  From linear regression, where line crosses RDF = 1: " + str(4*linear_regression_equals_y_1[1]) + " nm")
+
 
 # EOF #
